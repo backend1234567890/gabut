8ee78b58f2a7e2bb069279f9ab3fe9cb486de6e8
backend1234567890 <163655702+backend1234567890@users.noreply.github.com>
2024-04-01 22:45:37 +1100

    Update dataStore.js

--- dataStore.js
+++ dataStore.js
@@ -14,20 +14,24 @@ export function getData () {
         });
 }
 
-export function setData (newData) {
-    data = newData;
+export function setData(newData) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "connect.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                console.log(xhr.responseText);
+                try {
+                    let response = JSON.parse(xhr.responseText);
+                    console.log("Received data:", response);
+                } catch (error) {
+                    console.error("Error parsing JSON:", error);
+                }
             } else {
                 console.error("Error posting data: " + xhr.status);
             }
         }
     };
-    let jsonData = JSON.stringify({data}); // Send just the 'number' property
-    xhr.send(jsonData);
+    let jsonData = JSON.stringify({ data: newData }); // Send just the 'data' property
+    xhr.send("data=" + encodeURIComponent(jsonData)); // Send data in the request body
 }
